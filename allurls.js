857bc79f5ab93b9858442e1239e9d9417f7f51a5
Elena Dolgova <ms.Elena.Dolgova@ya.ru>
2021-03-17 00:48:19 +0300

    gg

--- allurls.js
+++ allurls.js
@@ -1,10 +1,13 @@
 
 
 function loadPage() {
-    storage.get(null, function (result) {
-        console.log('try to get in load page', result);
-    });
+    return "Ggggg";
+    // return storage.get(null, function (result) {
+    //     console.log('try to get in load page', result);
+    // });
     // chrome.tabs.executeScript({
     //   file: 'alert.js'
     // }); 
   }
+  
+  document.getElementById('p1').addEventListener('click', loadPage);
